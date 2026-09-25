dd3791c77319eac6dc14c0f50c47bade7f4492d7
YgorPereira <ygorrp25@gmail.com>
2026-09-24 21:37:57 -0300

    atualizando repo

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import Button from '../Buttons/Button/Button'
+
+import styles from './Sidebar.module.css'
+
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
